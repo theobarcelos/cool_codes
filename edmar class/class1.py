2e85c088e354914789730d30f82bc6f99417c490
Theo Marchetti Barcelos <barcelostheo5@gmail.com>
2024-09-25 08:38:57 -0300

    Update class1.py

--- edmar class/class1.py
+++ edmar class/class1.py
@@ -1,3 +1,17 @@
+# Inserindo Flask
 from flask import Flask, render_template, request, redirect, url_for
+
+#Inserindo MySQL
 import mysql.connector
 from mysql.connector import Error
+
+app = Flask(__name__)
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host = "localhost",
+            database = "escola",
+            user = "root",
+            password = ""
+        )
