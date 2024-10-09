63790c046d75a40b7c8c7ce7ddcf17f2dbaedbb8
Theo Marchetti Barcelos <barcelostheo5@gmail.com>
2024-10-09 01:05:31 -0300

    Update main3.py

--- senac-activities/main3.py
+++ senac-activities/main3.py
@@ -5,8 +5,8 @@ class Livro:
         self.num_copias = num_copias
 
 livro1 = Livro('Senhor dos Anéis', 'J. R. R. Tolkien', 14)
-livro2 = Livro('A Sutil Arte de Ligar o F*da-se', 'Mark Manson', 0 )
-livro3 = Livro()
+livro2 = Livro('A Sutil Arte de Ligar o F*da-se', 'Mark Manson', 0)
+livro3 = Livro("Sobrevivendo no Inferno', 'Racionais MC's", 1)
 
 class Usuario:
     def __init__(self, nome, num_inscricao, livros_emprestados):
@@ -14,10 +14,37 @@ class Usuario:
         self.num_inscricao = num_inscricao
         self.livros_emprestados = livros_emprestados
 
-class Emprestimo: 
-    def __init__(self, nome_emprest, livro_emprest):
-        self.nome_emprest = nome_emprest
-        self.livro_emprest = livro_emprest
+leitor1 = Usuario('Tiago', 4600, 1)
+leitora1 = Usuario('Suzy', 1302, 0)
+leitor2 = Usuario('João Vitor', 9301, 2)
 
-    def emprestar_livro(self):
-        nome
+class Emprestimo: 
+    def __init__(self):
+        ...
+    def emprestar_livro():
+        pergunta_livro = input('Qual livro você gostaria de pegar? ')
+        pergunta_num = input('Qual o seu número de inscrição? ')
+
+        if pergunta_num == Usuario.num_inscricao:
+            if pergunta_livro == Livro.titulo:
+                if Livro.num_copias == 0:
+                    print(f'Desculpe {Usuario.nome}, mas não possuímos nenhuma cópia de {Livro.titulo} no momento!')
+                elif Usuario.livros_emprestados == 2:
+                    print('Você atingiu o limite de 2 livros por pessoa!')
+                else:
+                    print(f'Possuímos {Livro.num_copias} de unidades do livro {Livro.titulo}')
+                    Livro.num_copias -= 1
+                    Usuario.livros_emprestados += 1
+                    pergunta_crucial = input('Você gostaria de adquirir outro de nossos livros?')
+                    if Usuario.livros_emprestados == 2:
+                        print('Você atingiu o limite de 2 livros por pessoa!')
+                    else:
+                        if Livro.num_copias == 0:
+                            print('Parabéns! Você pegou a última unidade do livro para alugar. Que sorte!')
+                        else: 
+                            print(f'Ainda resta(m) {Livro.num_copias} de unidades do livro. Gostaria de pegar mais uma?')
+            else:
+                print('Foi mal! Não possuímos este livro em nosso catálogo!')
+        else:
+            print(f'Não possuímos nenhum número de identificação {Usuario.num_inscricao} em nosso banco de dados!')
+
