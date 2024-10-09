ebedea4b24d4197874d19a076f4ca31ec4bd0925
Theo Marchetti Barcelos <barcelostheo5@gmail.com>
2024-10-09 08:05:57 -0300

    Update main3.py

--- senac-activities/main3.py
+++ senac-activities/main3.py
@@ -6,12 +6,12 @@ class Livro:
 
 livro1 = Livro('Senhor dos Anéis', 'J. R. R. Tolkien', 14)
 livro2 = Livro('A Sutil Arte de Ligar o F*da-se', 'Mark Manson', 0)
-livro3 = Livro("Sobrevivendo no Inferno', 'Racionais MC's", 1)
+livro3 = Livro('Sobrevivendo no Inferno', "Racionais MC's", 1)
 
 class Usuario:
     def __init__(self, nome, num_inscricao, livros_emprestados):
         self.nome = nome
-        self.num_inscricao = num_inscricao
+        self.num_inscricao = int(num_inscricao)
         self.livros_emprestados = livros_emprestados
 
 leitor1 = Usuario('Tiago', 4600, 1)
@@ -22,29 +22,108 @@ class Emprestimo:
     def __init__(self):
         ...
     def emprestar_livro():
-        pergunta_livro = input('Qual livro você gostaria de pegar? ')
         pergunta_num = input('Qual o seu número de inscrição? ')
 
-        if pergunta_num == Usuario.num_inscricao:
-            if pergunta_livro == Livro.titulo:
-                if Livro.num_copias == 0:
-                    print(f'Desculpe {Usuario.nome}, mas não possuímos nenhuma cópia de {Livro.titulo} no momento!')
-                elif Usuario.livros_emprestados == 2:
-                    print('Você atingiu o limite de 2 livros por pessoa!')
-                else:
-                    print(f'Possuímos {Livro.num_copias} de unidades do livro {Livro.titulo}')
-                    Livro.num_copias -= 1
-                    Usuario.livros_emprestados += 1
-                    pergunta_crucial = input('Você gostaria de adquirir outro de nossos livros?')
-                    if Usuario.livros_emprestados == 2:
-                        print('Você atingiu o limite de 2 livros por pessoa!')
+        if pergunta_num == leitor1.num_inscricao:
+            print(f'Bem-Vindo(a) de novo, {leitor1.nome}!')
+            pergunta_livro = input('Qual livro você gostaria de pegar? ')
+            if leitor1.livros_emprestados == 2:
+                print('Você atingiu o limite de 2 livros por pessoa!')
+            else: 
+                if pergunta_livro == livro1.titulo:
+                    if livro1.num_copias == 0:
+                        print(f'Desculpe {leitor1.nome}, mas não possuímos nenhuma cópia de {livro1.titulo} no momento!')
                     else:
-                        if Livro.num_copias == 0:
-                            print('Parabéns! Você pegou a última unidade do livro para alugar. Que sorte!')
-                        else: 
-                            print(f'Ainda resta(m) {Livro.num_copias} de unidades do livro. Gostaria de pegar mais uma?')
-            else:
-                print('Foi mal! Não possuímos este livro em nosso catálogo!')
+                        print(f'Possuímos {livro1.num_copias} de unidades do livro {livro1.titulo}')
+                        livro1.num_copias -= 1
+                        leitor1.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitor1.nome}: {leitor1.livros_emprestados}')
+                        print(f'Número de cópias de {livro1.titulo} restantes: {livro1.num_copias}')
+                elif pergunta_livro == livro2.titulo:
+                    if livro2.num_copias == 0:
+                        print(f'Desculpe {leitor1.nome}, mas não possuímos nenhuma cópia de {livro2.titulo} no momento!')
+                    else:
+                        print(f'Possuímos {livro2.num_copias} de unidades do livro {livro2.titulo}')
+                        livro2.num_copias -= 1
+                        leitor1.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitor1.nome}: {leitor1.livros_emprestados}')
+                        print(f'Número de cópias de {livro2.titulo} restantes: {livro2.num_copias}')
+                else:
+                    if livro3.num_copias == 0:
+                        print(f'Desculpe {leitor1.nome}, mas não possuímos nenhuma cópia de {livro3.titulo} no momento!')
+                    else:
+                        print(f'Possuímos {livro3.num_copias} de unidades do livro {livro3.titulo}')
+                        livro3.num_copias -= 1
+                        leitor1.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitor1.nome}: {leitor1.livros_emprestados}')
+                        print(f'Número de cópias de {livro3.titulo} restantes: {livro3.num_copias}')
+        elif pergunta_num == leitor2.num_inscricao:
+            print(f'Bem-Vindo(a) de novo, {leitor2.nome}!')
+            pergunta_livro = input('Qual livro você gostaria de pegar? ')
+            if leitor2.livros_emprestados == 2:
+                print('Você atingiu o limite de 2 livros por pessoa!')
+            else: 
+                if pergunta_livro == livro1.titulo:
+                    if livro1.num_copias == 0:
+                        print(f'Desculpe {leitor2.nome}, mas não possuímos nenhuma cópia de {livro1.titulo} no momento!')
+                    else:
+                        print(f'Possuímos {livro1.num_copias} de unidades do livro {livro1.titulo}')
+                        livro1.num_copias -= 1
+                        leitor2.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitor2.nome}: {leitor2.livros_emprestados}')
+                        print(f'Número de cópias de {livro1.titulo} restantes: {livro1.num_copias}')
+                elif pergunta_livro == livro2.titulo:
+                    if livro2.num_copias == 0:
+                        print(f'Desculpe {leitor2.nome}, mas não possuímos nenhuma cópia de {livro2.titulo} no momento!')
+                    else:
+                        print(f'Possuímos {livro2.num_copias} de unidades do livro {livro2.titulo}')
+                        livro2.num_copias -= 1
+                        leitor2.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitor2.nome}: {leitor2.livros_emprestados}')
+                        print(f'Número de cópias de {livro2.titulo} restantes: {livro2.num_copias}')
+                else:
+                    if livro3.num_copias == 0:
+                        print(f'Desculpe {leitor2.nome}, mas não possuímos nenhuma cópia de {livro3.titulo} no momento!')
+                    else:
+                        print(f'Possuímos {livro3.num_copias} de unidades do livro {livro3.titulo}')
+                        livro3.num_copias -= 1
+                        leitor2.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitor2.nome}: {leitor2.livros_emprestados}')
+                        print(f'Número de cópias de {livro3.titulo} restantes: {livro3.num_copias}')
+        elif pergunta_num == leitora1.num_inscricao:
+            print(f'Bem-Vindo(a) {leitora1.nome}!')
+            pergunta_livro = input('Qual livro você gostaria de pegar? ')
+            if leitora1.livros_emprestados == 2:
+                print('Você atingiu o limite de 2 livros por pessoa!')
+            else: 
+                if pergunta_livro == livro1.titulo:
+                    if livro1.num_copias == 0:
+                        print(f'Desculpe {leitora1.nome}, mas não possuímos nenhuma cópia de {livro1.titulo} no momento!')
+                    else:
+                        print(f'Possuímos {livro1.num_copias} de unidades do livro {livro1.titulo}')
+                        livro1.num_copias -= 1
+                        leitora1.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitora1.nome}: {leitora1.livros_emprestados}')
+                        print(f'Número de cópias de {livro1.titulo} restantes: {livro1.num_copias}')
+                elif pergunta_livro == livro2.titulo:
+                    if livro2.num_copias == 0:
+                        print(f'Desculpe {leitora1.nome}, mas não possuímos nenhuma cópia de {livro2.titulo} no momento!')
+                    else:
+                        print(f'Possuímos {livro2.num_copias} de unidades do livro {livro2.titulo}')
+                        livro2.num_copias -= 1
+                        leitora1.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitora1.nome}: {leitora1.livros_emprestados}')
+                        print(f'Número de cópias de {livro2.titulo} restantes: {livro2.num_copias}')
+                else:
+                    if livro3.num_copias == 0:
+                        print(f'Desculpe {leitora1.nome}, mas não possuímos nenhuma cópia de {livro3.titulo} no momento!')
+                    else:
+                        print(f'Possuímos {livro3.num_copias} de unidades do livro {livro3.titulo}')
+                        livro3.num_copias -= 1
+                        leitora1.livros_emprestados += 1
+                        print(f'Número de livros pegos por {leitora1.nome}: {leitora1.livros_emprestados}')
+                        print(f'Número de cópias de {livro3.titulo} restantes: {livro3.num_copias}')
         else:
-            print(f'Não possuímos nenhum número de identificação {Usuario.num_inscricao} em nosso banco de dados!')
+            print(f'Não possuímos nenhum número de identificação {pergunta_num} em nosso banco de dados!')
 
+print(Emprestimo.emprestar_livro())
